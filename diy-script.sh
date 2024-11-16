4beee805231cecdc38b3d72ec58cb0a939ea6e31
oppen321 <zj18139624826@gmail.com>
2024-11-16 10:24:46 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # 修改默认IP
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/luci2/bin/config_generate
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
